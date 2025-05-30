ebd3baeaf5dc3913db287351043c69ac5949c0c2
Jayasudha12 <jayasudha.t2022cse@sece.ac.in>
2025-05-31 01:54:56 +0530

    basic setup

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:3000','https://taskflowapplication.netlify.app'];
+const allowedOrigins = ['http://localhost:3000','https://taskflowapplication2.netlify.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
